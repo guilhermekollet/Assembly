388b59382ba319b08aa42d139de500e5e658841d
Guilherme Serafim Kollet <40221574+guilhermekollet@users.noreply.github.com>
2022-06-29 14:52:04 -0300

    alpha 2.0
    Update highLevel.cpp

--- highLevel.cpp
+++ highLevel.cpp
@@ -5,58 +5,124 @@ using namespace std;
 
 int main (){
     int vetA [] = {810 ,100, 560, 380, 600, 87};
-    int vetB [] = {555, 817 ,124, 890 ,456};
+    int vetB [] = {800, 555, 817, 124, 890, 456};
     int vetC [] = {345, 200, 700, 180, 600, 490};
     int vetD[10];
     
-    int sumA = 0 , sumB = 0 , sumC = 0; 
-    int n = 6;
-    int min = 419;
-    int k = 0;
+    int N = 6;
+    int K = 0;
+    int I = 0;
+    int MIN = 1000;
 
-    for (int i = 0; i < 6; i++)
+    int sumA = 0;
+    int sumB = 0;
+    int sumC = 0;
+
+    int mediaA = 0;
+    int mediaB = 0;
+    int mediaC = 0;
+
+    cout << "Vetor A: ";
+
+    for (int i=0; i < N; i++)
     {
-        if(vetA[i] < min) 
-        {
-            cout << "valor menor encontrado no vetor A: " << vetA[i] << endl;
-            vetD[k] = vetA[i];
-            k++;
-        }
+
+        cout << vetA[i] << " ";
 
     }
 
-    for (int i = 0; i < 6; i++)
+    cout << endl << "Vetor B: ";
+
+    for (int i=0; i < N; i++)
     {
-        if(vetB[i] < min) 
-        {
-            cout << "valor menor encontrado no vetor B: " << vetB[i] << endl;
-            vetD[k] = vetB[i];
-            k++;
-        }
+
+        cout << vetB[i] << " ";
 
     }
 
-    for (int i = 0; i < 6; i++)
+    cout << endl << "Vetor C: ";
+
+    for (int i=0; i < N; i++)
     {
-        if(vetC[i] < min) 
+
+        cout << vetC[i] << " ";
+
+    }
+
+    cout << endl;
+    cout << "N: " << N << endl;
+    cout << endl;
+
+    while(I < N)
+    {
+        sumA += vetA[I];
+        sumB += vetB[I];
+        sumC += vetC[I];
+
+        I++;
+    }
+
+    mediaA = sumA / N;
+    mediaB = sumB / N;
+    mediaC = sumC / N;
+
+    cout << "Somatorio A: " << sumA << " media A: " << mediaA << endl;
+    cout << "Somatorio B: " << sumB << " media B: " << mediaB << endl;
+    cout << "Somatorio C: " << sumC << " media C: " << mediaC << endl;
+
+    cout << endl;
+
+    if(mediaA < MIN)
+    MIN = mediaA;
+
+    if(mediaB < MIN)
+    MIN = mediaB;
+
+    if(mediaC < MIN)
+    MIN = mediaC;
+
+    cout << "Minimo: " << MIN << endl;
+
+    cout << endl;
+
+    I = 0;
+
+    while(I < N)
+    {
+        if(vetA[I] < MIN) 
         {
-            cout << "valor menor encontrado no vetor C: " << vetC[i] << endl;
-            vetD[k] = vetC[i];
-            k++;
+            vetD[K] = vetA[I];
+            K++;
         }
+
+        if(vetB[I] < MIN) 
+        {
+            vetD[K] = vetB[I];
+            K++;
+        }
+
+        if(vetC[I] < MIN) 
+        {
+            vetD[K] = vetC[I];
+            K++;
+        }
+
+        I++;
 
     }
 
     cout << "vetor D: ";
 
-    for (int i=0; i<k; i++)
+    for (int i=0; i < K; i++)
     {
 
         cout << vetD[i] << " ";
 
     }
+
     cout << endl;
-    cout << "k: " << k << endl;
+
+    cout << "K: " << K << endl;
     return 0 ;
 }
 
