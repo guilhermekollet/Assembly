f93fc3a67bcbbb5ffc272088316ea75637540fef
Bruno Thofehrn <51756037+Bruno-Thofehrn@users.noreply.github.com>
2022-06-29 11:17:21 -0300

    alpha 0.1

--- highLevel.cpp
+++ highLevel.cpp
@@ -1,22 +1,62 @@
-#include #include
+#include <iostream>
+#include  <string>
+
 using namespace std;
 
-int main ()
-{
+int main (){
+    int vetA [] = {810 ,100, 560, 380, 600, 87};
+    int vetB [] = {555, 817 ,124, 890 ,456};
+    int vetC [] = {345, 200, 700, 180, 600, 490};
+    int vetD[10];
+    
+    int sumA = 0 , sumB = 0 , sumC = 0; 
+    int n = 6;
+    int min = 419;
+    int k = 0;
 
-	int vetA [] = {810 ,100, 560, 380, 600, 87};
-	int vetB [] = {555, 817 ,124, 890 ,456};
-	int vetC [] = {345, 200, 700, 180, 600, 490};
+    for (int i = 0; i < 6; i++)
+    {
+        if(vetA[i] < min) 
+        {
+            cout << "valor menor encontrado no vetor A: " << vetA[i] << endl;
+            vetD[k] = vetA[i];
+            k++;
+        }
 
-	int sumA = 0 , sumB = 0 , sumC = 0;
-	int n = 6;
+    }
 
-	for (int i = 0; i < 6; i++)
-	{
-		sumA += vetA[i];
-	}
+    for (int i = 0; i < 6; i++)
+    {
+        if(vetB[i] < min) 
+        {
+            cout << "valor menor encontrado no vetor B: " << vetB[i] << endl;
+            vetD[k] = vetB[i];
+            k++;
+        }
 
-	cout << sumA << endl;
+    }
 
-return 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if(vetC[i] < min) 
+        {
+            cout << "valor menor encontrado no vetor C: " << vetC[i] << endl;
+            vetD[k] = vetC[i];
+            k++;
+        }
+
+    }
+
+    cout << "vetor D: ";
+
+    for (int i=0; i<k; i++)
+    {
+
+        cout << vetD[i] << " ";
+
+    }
+    cout << endl;
+    cout << "k: " << k << endl;
+    return 0 ;
 }
+
